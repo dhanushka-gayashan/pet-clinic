5e2fc343174a2c7597826e214d081b400dd1012f
Dhanushka Gayashan <dhanushka.kohombange@tenzing.co.nz>
2020-06-06 18:46:12 +1200

    Implemented Service Layer

---
+++ pet-clinic-data/src/main/java/com/kdg/petclinic/services/OwnerService.java
@@ -0,0 +1,16 @@
+package com.kdg.petclinic.services;
+
+import com.kdg.petclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
---
+++ pet-clinic-data/src/main/java/com/kdg/petclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package com.kdg.petclinic.services;
+
+import com.kdg.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
